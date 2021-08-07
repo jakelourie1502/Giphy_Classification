d4e507269c4917937445dfac5412c0aea4dde230
Ubuntu <ubuntu@ip-172-31-47-253.eu-west-2.compute.internal>
2021-08-07 20:51:17 +0000

    ads changes

--- Testing_model.py
+++ Testing_model.py
@@ -18,29 +18,36 @@ import numpy as np
 from PIL import Image
 
 dict_of_labels = {0:'Hugging', 1:'Fighting', 2: 'Falling'}
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 train_folder = '/home/ubuntu/padded_ims/train/'
 val_folder = '/home/ubuntu/padded_ims/val/'
 train_dataset, val_dataset = VidDataset(train_folder), VidDataset(val_folder)
-print(train_dataset[100][0].shape)
 train_dataloader = DataLoader(batch_size = 1,dataset= train_dataset,shuffle=True, num_workers=1)
 val_dataloader = DataLoader(batch_size = 1,dataset= val_dataset,shuffle=True,num_workers=1)
 model = SlowNet(number_of_classes = 3).to(device)
 # input_path_to_latest_model = input('input path to latest model')
-model = torch.load('/home/ubuntu/giphy_project_files/model_checkpoints/Modelsave4EPOCHEND.pt')
+model = torch.load('/home/ubuntu/new_github_folder/Giphy_Classification/model_checkpoint/Modelsave6-11775.pt')
 
 # number_of_example = int(input('how_many_examples?'))
 idx = 0
+output_labels_list = []
+target_labels = []
 for batch, label, targets in val_dataloader:
-    print(batch.shape)
     idx+=1
-    if idx > 5: break
+    if idx > 1000: break
     model.eval()
     targets = targets[0].cpu().item()
     batch=batch.permute(0,4,1,2,3)
-    print(batch.shape)
     batch = batch.to(device).float()
-    outputs = (model(batch))
+    outputs = model(batch)
     best = torch.argmax(outputs,dim=-1)[0].cpu().item()
-    print(f'output: {best} {dict_of_labels[best]} targets: {targets} {dict_of_labels[targets]} {label}')
+    target_labels.append(label)
+    output_labels_list.append(dict_of_labels[best])
+    # print(outputs)
+    # print(f'output: {best} {dict_of_labels[best]} targets: {targets} {dict_of_labels[targets]} {label}')
+
+import pandas as pd
+y_actu = pd.Series(target_labels, name='Actual')
+y_pred = pd.Series(output_labels_list, name='Predicted')
+df_confusion = pd.crosstab(y_actu, y_pred)
+print(df_confusion)
